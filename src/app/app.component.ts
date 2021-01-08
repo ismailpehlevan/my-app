ba9b08c8554748fa6fd417fa0fa861cdf0bb25e9
ISMAIL PEHLEVAN <ismailpehlevan@icloud.com>
2021-01-08 21:51:41 +0300

    ngx toastr spinner added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  //Component'e dair herhangi bir işlemi buradan yapıyoruz.
+constructor(private toastr: ToastrService,private spinner:NgxSpinnerService ) {}
+
+ngOnInit() {
+  this.toastr.success("Sigarayı bıraktığın gibi! ve Dikkat dağıtıcardan  UZAK DUR!!","ASLA PES ETME!")
+  this.spinner.show();
+  setTimeout(() => {
+    this.spinner.hide();
+  },1500);
+}
   title = 'my-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxSpinnerModule } from 'ngx-spinner'
+
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
@@ -10,8 +16,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut:2000,
+      progressBar: true,
+      easing:"ease-in",
+      closeButton:false,
+      progressAnimation:"decreasing",
+      preventDuplicates:true,
+      positionClass:"toast-bottom-left"
+    }),
+    NgxSpinnerModule,
     AppRoutingModule
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
