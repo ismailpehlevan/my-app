eeaf323aa879e7bd7a4a67ee0bc960647f878888
ISMAIL PEHLEVAN <ismailpehlevan@icloud.com>
2021-01-08 22:06:46 +0300

    ignite ui added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxSpinnerModule } from 'ngx-spinner'
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { ToastrModule } from 'ngx-toastr';
-
 
 @NgModule({
   declarations: [
@@ -19,19 +18,20 @@ import { ToastrModule } from 'ngx-toastr';
     CommonModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
-      timeOut:2000,
+      timeOut: 2000,
       progressBar: true,
-      easing:"ease-in",
-      closeButton:false,
-      progressAnimation:"decreasing",
-      preventDuplicates:true,
-      positionClass:"toast-bottom-left"
+      easing: "ease-in",
+      closeButton: false,
+      progressAnimation: "decreasing",
+      preventDuplicates: true,
+      positionClass: "toast-bottom-left"
     }),
     NgxSpinnerModule,
     AppRoutingModule
   ],
-  schemas:[CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
